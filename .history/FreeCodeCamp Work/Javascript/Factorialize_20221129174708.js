53e6303343dbbd02fd73be8193a1f4c95c443a8d
RoshanArun3 <96964652+RoshanArun3@users.noreply.github.com>
2022-11-29 17:58:28 -0700

    JS update

---
+++ .history/FreeCodeCamp Work/Javascript/Factorialize_20221129174708.js
@@ -0,0 +1,13 @@
+function factorialize(num) {
+    for(let i = num - 1; i > 0; i--){
+      num = num * i;
+    }
+  
+    if(num == 0){
+      return 1;
+    }
+    console.log(num)
+    return num;
+  }
+  
+  factorialize(5);
